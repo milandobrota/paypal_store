7539ca5a8251df1c642a3e826d018d04675583ae
Milan Dobrota <elitecoding@gmail.com>
2009-11-08 22:11:38 +0100

    authlogic implemented

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,5 @@
 class StoreController < ApplicationController
   
-  layout nil
-  
   def index
     @products = Product.search(params[:search], params[:page])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  
+  map.login 'login', :controller => 'user_sessions', :action => 'new'  
+  map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
+  
+  map.resources :user_sessions
+
+  map.resources :users
+
   map.resources :categories
   map.resources :products
   map.resources :my_purchases
